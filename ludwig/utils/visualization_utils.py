9ba654d8d047115c3c24b65baba8097d7c132063
Aaron Glazer <aglazer@users.noreply.github.com>
2019-03-15 22:09:06 -0400

    Try to show plots inline

--- ludwig/utils/visualization_utils.py
+++ ludwig/utils/visualization_utils.py
@@ -14,6 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+
+%matplotlib inline
+
 from __future__ import absolute_import
 from __future__ import division
 
